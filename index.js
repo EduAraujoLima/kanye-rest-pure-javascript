69fdb2cf4f3a1de121fa1e5e38cbfadb8ee68a2f
EduAraujoLima <edu.wp@outlook.com>
2021-06-22 10:47:19 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+const apiUrl = 'https://api.kanye.rest/';
+let quote = '';
+
+document
+  .querySelector('.quote-button')
+  .addEventListener('click', () => getQuote());
+
+const getQuote = async () => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  quote = data.quote;
+
+  document.querySelector('.frase').innerHTML = quote;
+};
+
+getQuote();
